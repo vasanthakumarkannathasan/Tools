46b037da8f55c93de21a0c5749fcbf03fef216b0
VasanthaKumar Kannathasan <vasanthakumar.kannathasan@hp.com>
2022-03-04 18:28:02 +0530

    Base code

---
+++ Logout from all apps/Logout from all Apps/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logout_from_all_Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Do you want to logout all application");
+            Console.WriteLine("Press 1 for 'Yes'; Press anykey for 'No'");
+            Console.ReadLine();
+
+        }
+    }
+}
